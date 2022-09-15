478cc1a3a1115394af7f1c7443b9acdc2cdf4bf3
schnurluke <schnurle@mail.uc.edu>
2022-09-15 16:48:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main
+{
+
+        public static void main(String[] args)
+        {
+            int fall=125;
+            int summer=225;
+            int winter=5562;
+            int spring=420;
+            int total= spring + winter + summer+ fall;
+                System.out.println("The maintenance cost for the fall is "+ fall + "$ The maintenance cost for summer is " +
+                        summer + "$ The maintenance cost for the winter is "+ winter+ "$ The maintenance cost for the spring is "+spring+"$"+
+               " The total maintenance cost is "+ total+"$" );
+
+
+
+
+
+
+
+
+        }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
